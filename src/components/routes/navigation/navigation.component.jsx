dd42afe9f9ad4943c18800e922d4b83e386c271a
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-01-03 18:55:48 -0500

    Added a functional Navigational Bar and the Store logo

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react/cjs/react.production.min";
+import { Link, Outlet } from "react-router-dom";
+import {ReactComponent as CrwnLogo} from '../../../assests/crown.svg'
+import './navigation.styles.scss'
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+        <Link className="logo-container" to='/'>
+         <CrwnLogo className='logo' />
+        </Link>
+            <div className="links-container">
+                <Link className="nav-link" to='/shop/'>
+                    Shop
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+  }
+
+  export default Navigation
